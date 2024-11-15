3b1415b4df82700a33484be8b9edd300d12d56f7
amaliaaSorribes <as5628@drexel.edu>
2024-11-15 15:32:11 -0500

    :art: Updated navBar

--- js/script.js
+++ js/script.js
@@ -1,13 +1,3 @@
-function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-    x.style.display = "none";
-    } else {
-    x.style.display = "block";
-    }
-}
-
-
 const readMoreBtn = document.querySelector('.read-more-btn');
 function myFunction2() {
     var x = document.getElementById("moreText");
